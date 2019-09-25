b29090e53c6392cca367623080d37987a0cb64a4
nina-xu <nina.ning.xu@gmail.com>
2019-09-25 14:52:46 -0400

    first histogram
    I'm about to add the color change. we'll see

--- starter_app.py
+++ starter_app.py
@@ -18,28 +18,27 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-data = pd.read_csv('C:/Users/Ning/Insight material/Insight_College_Ranking_for_International_Students/Data/Data Merged.csv')
-institution = 'Bucknell University'
+data = pd.read_csv('C:/Users/Ning/Insight material/Insight_College_Ranking_for_International_Students/Data/Data national universities 274.csv')
+institution = 'Samford University'
 intl_pct = data.intl_pct[data.INSTNM == institution].tolist()[0]/100
 diverse_ind = data.diverse_ind[data.INSTNM == institution].tolist()[0]
 
 
 
 app.layout = html.Div([
-    html.H1(children='Colleges for International Students'),
+    html.H1(children='Universal University'),
 
     html.Div(children='''
-        A US college/univeristy ranking for international students
+        Boosting your international student acquisition 
     '''),
              
     dcc.Graph(
         id='institution-graph',
         figure={
             'data': [
-                go.Bar(
-                    x=['% Intl Students', 'Diversity Ind'],
-                    y=[intl_pct, diverse_ind],
-                    name=institution
+                go.Histogram(
+                    x=data.diverse_ind,
+                    histnorm='probability'
                 ) 
             ],
             'layout': {
